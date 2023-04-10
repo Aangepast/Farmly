f70cbb444de928e78d83127f4e486ff021795a03
BuildTools <unconfigured@null.spigotmc.org>
2023-04-10 08:45:59 +0200

    Outside Click Handler v2

--- src/main/java/dev/aangepast/farmly/listeners/onInventoryClickOutsideHandler.java
+++ src/main/java/dev/aangepast/farmly/listeners/onInventoryClickOutsideHandler.java
@@ -1,5 +1,6 @@
 package dev.aangepast.farmly.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -10,6 +11,8 @@ public class onInventoryClickOutsideHandler implements Listener {
     public void onOutsideInventoryClick(InventoryClickEvent e){
         if(e.getRawSlot()==-999){
             e.getWhoClicked().closeInventory();
+            Player player = (Player) e.getWhoClicked();
+            player.playSound(player.getLocation(), "block.stone_pressureplate.click_off",1,1);
         }
     }
 
